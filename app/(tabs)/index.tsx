ada93932aac418b4f1522fbc2916d2d76a8aa62e
Mitchell Brenner <mitchbrenner8@gmail.com>
2025-05-07 21:27:53 -0600

    Added apple maps

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,75 +1,164 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
-
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { locationList } from "@/LocationList";
+import { useBottomTabBarHeight } from "@react-navigation/bottom-tabs";
+import { useImage } from "expo-image";
+import { AppleMaps } from "expo-maps";
+import { AppleMapsMapType } from "expo-maps/build/apple/AppleMaps.types";
+import { useRef, useState } from "react";
+import { Button, Platform, StyleSheet, View } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function HomeScreen() {
-  return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
-        <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12',
-            })}
-          </ThemedText>{' '}
-          to open developer tools.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
-        <ThemedText>
-          {`Tap the Explore tab to learn more about what's included in this starter app.`}
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
-        <ThemedText>
-          {`When you're ready, run `}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+  const ref = useRef<AppleMaps.MapView>(null);
+  const bottom = useBottomTabBarHeight();
+  const [locationIndex, setLocationIndex] = useState(0);
+  const image = useImage(
+    "https://cdn.wiscweb.wisc.edu/wp-content/uploads/sites/83/2018/02/High-Res-Bucky-768x768.png",
+    {
+      onError(error) {
+        console.error("Error loading image:", error);
+      },
+    }
   );
+
+  const rederMapControls = () => (
+    <>
+      <View style={{ flex: 8 }} pointerEvents="none" />
+      <View style={styles.controlsContainer}>
+        <Button title="Prev" onPress={() => handleChangeWithRef("prev")} />
+        <Button title="Next" onPress={() => handleChangeWithRef("next")} />
+      </View>
+    </>
+  );
+
+  function handleChangeWithRef(direction: "next" | "prev") {
+    const newIndex = locationIndex + (direction === "next" ? 1 : -1);
+    const nextLocation = locationList[newIndex];
+
+    ref.current?.setCameraPosition({
+      coordinates: {
+        latitude: nextLocation.stores[0].point[0],
+        longitude: nextLocation.stores[0].point[1],
+      },
+      zoom: 12,
+    });
+
+    setLocationIndex(newIndex);
+  }
+
+  if (Platform.OS === "ios") {
+    return (
+      <>
+        <AppleMaps.View
+          ref={ref}
+          style={StyleSheet.absoluteFill}
+          cameraPosition={{
+            coordinates: {
+              latitude: locationList[locationIndex].stores[0].point[0],
+              longitude: locationList[locationIndex].stores[0].point[1],
+            },
+            zoom: 12,
+          }}
+          markers={markersApple}
+          annotations={[
+            {
+              coordinates: { latitude: 49.2597, longitude: -123.1207 },
+              title: "BUCKY",
+              // text: "Expo HQ",
+              textColor: "white",
+              backgroundColor: "black",
+              icon: image ? image : undefined,
+            },
+          ]}
+          properties={{
+            isTrafficEnabled: false,
+            mapType: AppleMapsMapType.STANDARD,
+            selectionEnabled: true,
+          }}
+          polylines={[
+            {
+              color: "blue",
+              width: 5,
+              coordinates: [
+                { latitude: 49.2597, longitude: -123.1207 },
+                { latitude: 49.268034, longitude: -123.154819 },
+                { latitude: 49.286036, longitude: -123.12303 },
+                { latitude: 49.311879, longitude: -123.079241 },
+              ],
+            },
+          ]}
+          onMapClick={(e) => {
+            console.log(JSON.stringify({ type: "onMapClick", e }, null, 2));
+          }}
+          onMarkerClick={(e) => {
+            console.log(JSON.stringify({ type: "onMarkerClick", e }, null, 2));
+          }}
+          onCameraMove={(e) => {
+            console.log(JSON.stringify({ type: "onCameraMove", e }, null, 2));
+          }}
+          onPolylineClick={(e) => {
+            console.log(
+              JSON.stringify({ type: "onPolylineClick", e }, null, 2)
+            );
+          }}
+        />
+        <SafeAreaView
+          style={{ flex: 1, paddingBottom: bottom }}
+          pointerEvents="box-none" // allow touch events to pass through to the map
+        >
+          {rederMapControls()}
+        </SafeAreaView>
+      </>
+    );
+    // } else if (Platform.OS === "android") {
+    //   return <GoogleMaps.View style={{ flex: 1 }} />; // for this example, I did not set up the API key
+    // } else {
+    //   return <Text>Maps are only available on Android and IOS</Text>;
+  }
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
+  controlsContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
     gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
+    backgroundColor: "rgba(0, 0, 0, 0.8)",
   },
 });
+
+const markersApple = [
+  // {
+  //   coordinates: { latitude: 49.259133, longitude: -123.10079 },
+  //   title: "49th Parallel Café & Lucky's Doughnuts - Main Street",
+  //   tintColor: "brown",
+  //   systemImage: "cup.and.saucer.fill",
+  // },
+  {
+    coordinates: { latitude: 49.268034, longitude: -123.154819 },
+    title: "49th Parallel Café & Lucky's Doughnuts - 4th Ave",
+    tintColor: "brown",
+    systemImage: "cup.and.saucer.fill",
+  },
+  {
+    coordinates: { latitude: 49.286036, longitude: -123.12303 },
+    title: "49th Parallel Café & Lucky's Doughnuts - Thurlow",
+    tintColor: "brown",
+    systemImage: "cup.and.saucer.fill",
+  },
+  {
+    coordinates: { latitude: 49.311879, longitude: -123.079241 },
+    title: "49th Parallel Café & Lucky's Doughnuts - Lonsdale",
+    tintColor: "brown",
+    systemImage: "cup.and.saucer.fill",
+  },
+  {
+    coordinates: {
+      latitude: 49.27235336018808,
+      longitude: -123.13455838338278,
+    },
+    title: "A La Mode Pie Café - Granville Island",
+    tintColor: "orange",
+    systemImage: "fork.knife",
+  },
+];
